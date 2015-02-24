5a0a54c36126e9685f0323e1f237d11bb35cfb5e
Joe Van Dyk <joe@fixieconsulting.com>
2015-02-23 16:49:49 -0800

    Add comments

--- lib/tanga_services/logger.rb
+++ lib/tanga_services/logger.rb
@@ -2,6 +2,12 @@ require 'syslog/logger'
 require 'json'
 
 module TangaServices
+  # Logs json to syslog.
+  # Automatically delegates stuff to the syslog logger.
+  # Use like:
+  #    TangaServices.logger.open('my_application_name')
+  #    TangaServices.logger.info({message: "I'm interesting data"})
+  #    TangaServices.logger.error({message: "i crashed"})
   class Logger < Syslog::Logger
     def self.open(application_name)
       @logger ||= Syslog::Logger.new(application_name, Syslog::LOG_LOCAL7)
