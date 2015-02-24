dcb9848e73a1832aa31ed4ec20f291494fb8d922
Joe Van Dyk <joe@fixieconsulting.com>
2015-02-23 17:06:41 -0800

    define each of the methods that are ok to use on logger.

--- lib/tanga_services/logger.rb
+++ lib/tanga_services/logger.rb
@@ -9,13 +9,35 @@ module TangaServices
   #    TangaServices.logger.info({message: "I'm interesting data"})
   #    TangaServices.logger.error({message: "i crashed"})
   class Logger < Syslog::Logger
-    def self.open(application_name)
+    def self.application_name=(application_name)
       @logger ||= Syslog::Logger.new(application_name, Syslog::LOG_LOCAL7)
     end
 
-    def self.method_missing(method, data, &block)
-      log = { level: method, data: data }
-      @logger.send(method, log.to_json, &block)
+    def self.debug(hash)
+      log(:debug, hash)
+    end
+
+    def self.info(hash)
+      log(:info, hash)
+    end
+
+    def self.warn(hash)
+      log(:warn, hash)
+    end
+
+    def self.error(hash)
+      log(:error, hash)
+    end
+
+    def self.fatal(hash)
+      log(:fatal, hash)
+    end
+
+    def self.log(level, hash)
+      fail ArgumentError, 'must have application_name set' unless @logger
+      fail ArgumentError, 'we just log hashes' unless hash.is_a?(Hash)
+      data = { level: level, object: hash }
+      @logger.send(level, data.to_json)
     end
   end
 
--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -1,13 +1,39 @@
 require 'spec_helper'
 
 describe TangaServices do
+  before do
+    # Clears the logging variable.
+    TangaServices::Logger.instance_variable_set(:@logger, nil)
+  end
+
+  context '.application_name=' do
+    it 'should open the log with application name' do
+      logger = instance_double(Syslog::Logger)
+      expect(Syslog::Logger).to receive(:new).with('my_app', Syslog::LOG_LOCAL7).and_return(logger)
+      TangaServices.logger.application_name = 'my_app'
+    end
+  end
+
   context '.log' do
-    it 'should log to syslog' do
-      logger = instance_double(Syslog::Logger, info: 'sup')
-      expect(logger).to receive(:info).with({level: 'info', data: { joe: 'all right' } }.to_json)
-      expect(Syslog::Logger).to receive(:new).and_return(logger)
-      TangaServices.logger.open('my_app')
-      TangaServices.logger.info joe: 'all right'
+    it 'requires you log a hash' do
+      TangaServices.logger.application_name = 'app'
+      expect{TangaServices.logger.info('not a hash')}.to \
+        raise_error(ArgumentError, "we just log hashes")
+    end
+
+    it 'makes you set application name before' do
+      expect{TangaServices.logger.info('not a hash')}.to \
+        raise_error(ArgumentError, "must have application_name set")
+    end
+
+    %w( debug info warn error fatal ).each do |method|
+      it "should log #{ method}" do
+        logger = instance_double(Syslog::Logger)
+        expect(Syslog::Logger).to receive(:new).with('my_app', Syslog::LOG_LOCAL7).and_return(logger)
+        expect(logger).to receive(method).with({level: method, object: { joe: 'all right' } }.to_json)
+        TangaServices.logger.application_name = 'my_app'
+        TangaServices.logger.send(method, joe: 'all right')
+      end
     end
   end
 end
